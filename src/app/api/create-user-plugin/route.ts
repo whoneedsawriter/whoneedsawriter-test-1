fa254e3c312c30c5bdf7049abd5613c0068a732e
pabitrabarua123 <pabitrabesus@gmail.com>
2026-04-22 19:08:32 +0530

    user creation by plugin 2

--- src/app/api/create-user-plugin/route.ts
+++ src/app/api/create-user-plugin/route.ts
@@ -4,12 +4,36 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function  POST(req: NextRequest) {
 
-    const { email, name } = await req.json();
-    const user = await prismaClient.user.create({
-        data: {
-            email,
-            name,
-        },
+    try {
+    // transaction start
+    const result = await prismaClient.$transaction(async (tx) => {
+        const { email, name } = await req.json();
+        const user = await tx.user.create({
+            data: {
+                email,
+                name,
+            },
+        });
+
+        // create OTP
+        const otp = Math.floor(100000 + Math.random() * 900000);
+        await tx.optVerification.create({
+            data: {
+                userId: user.id,
+                email: email,
+                opt: otp,
+            },
+        });
+
+        return user;
     });
-    return NextResponse.json({ user });
+
+    if (result instanceof Error) {
+        return NextResponse.json({ success: false, message: result.message }, { status: 500 });
+    }
+    return NextResponse.json({ success: true, message: "User created successfully" }, { status: 200 });
+} catch (error) {
+    console.error("Error creating user:", error);
+    return NextResponse.json({ success: false, message: "User creation failed" }, { status: 500 });
+    }
 }
